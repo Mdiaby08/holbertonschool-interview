577de6c1ad437e5cc66a24298ddc2f6b54a4332a
Mdiaby08 <diabymassire93@gmail.com>
2026-09-11 16:39:29 +0200

    Fix formatting and improve Pascal's triangle function

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,16 +1,25 @@
 #!/usr/bin/python3
+"""
+Function that returns Pascal's triangle of n
+"""
+
 def pascal_triangle(n):
+    """
+    Return a list of lists representing Pascal's triangle of n
+    """
     if n <= 0:
         return []
- triangle = [[1]]
+
+    triangle = [[1]]
 
     for i in range(1, n):
         row = [1]
         prev = triangle[-1]
-for j in range(1, i):
+
+        for j in range(1, i):
             row.append(prev[j - 1] + prev[j])
-    
-  row.append(1)
+
+        row.append(1)
         triangle.append(row)
 
     return triangle
