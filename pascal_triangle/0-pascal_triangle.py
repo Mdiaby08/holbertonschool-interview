07afe9ed0b3ff649a318455fbcd445b22028b971
Mdiaby08 <diabymassire93@gmail.com>
2026-09-11 16:43:41 +0200

    Refactor Pascal's triangle generation logic

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -10,17 +10,17 @@ def pascal_triangle(n):
     """
     if n <= 0:
         return []
+
     triangle = [[1]]
-    
+
     for i in range(1, n):
         row = [1]
-        
         prev = triangle[-1]
+
         for j in range(1, i):
             row.append(prev[j - 1] + prev[j])
-            
+
         row.append(1)
-        
         triangle.append(row)
-        
+
     return triangle
