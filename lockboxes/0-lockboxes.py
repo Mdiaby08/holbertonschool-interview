7035f372e1f11acfabc5936cb6e37552abc9ab15
Mdiaby08 <diabymassire93@gmail.com>
2026-09-15 16:41:14 +0200

    Add canUnlockAll function to unlock boxes
    Implement function to determine if all boxes can be unlocked.

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -1,1 +1,14 @@
+#!/usr/bin/python3
+def canUnlockAll(boxes):
+    opened = [0]                 # on commence avec la boîte 0
+    keys = boxes[0].copy()       # clés trouvées dans la boîte 0
+
+    for key in keys:
+        if key < len(boxes) and key not in opened:
+            opened.append(key)
+            for new_key in boxes[key]:
+                if new_key not in keys:
+                    keys.append(new_key)
+
+    return opened
 
