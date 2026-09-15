d8ea86b80ed232e38a3aaa65f048d4f8da38fa23
Mdiaby08 <diabymassire93@gmail.com>
2026-09-15 16:49:08 +0200

    Add test cases for canUnlockAll function
    Added test cases for the canUnlockAll function.

--- lockboxes/0-main.py
+++ lockboxes/0-main.py
@@ -1,3 +1,4 @@
+carrie@ubuntu:~/lockboxes$ cat main_0.py
 #!/usr/bin/python3
 
 canUnlockAll = __import__('0-lockboxes').canUnlockAll
@@ -10,3 +11,5 @@ print(canUnlockAll(boxes))
 
 boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
 print(canUnlockAll(boxes))
+
+carrie@ubuntu:~/lockboxes$
